a5f4a6c53eda46fc61a9330c8477676f47d124b1
tomkoker <teddy.koker@gmail.com>
2017-04-26 17:40:34 -0400

    home page

--- client/main.js
+++ client/main.js
@@ -6,12 +6,14 @@ import Buy from './components/Buy.vue'
 import Login from './components/Login.vue'
 import Sell from './components/Sell.vue'
 import Signup from './components/Signup.vue'
+import Home from './components/Home.vue'
 
 Vue.use(VueRouter)
 
 let router = new VueRouter({
   routes: [
-    { path: '/buy', component: Buy },
+    { path: '/', component: Home},
+    { path: '/buy', component: Buy},
     { path: '/sell', component: Sell},
     { path: '/login', component: Login},
     { path: '/signup', component: Signup}
