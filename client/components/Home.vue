f872f0b975c461f1ca885a56de62e7527113f3a2
tomkoker <teddy.koker@gmail.com>
2017-04-26 17:40:46 -0400

    home page

---
+++ client/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="jumbotron">
+    <h2>Welcome</h2>
+    <p>Bookswitch is the best way to buy and sell textbooks with your classmates.</p>
+  </div>
+</template>
+<script>
+
+</script>
