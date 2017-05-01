403ba50f1b3d29427fbe11b963e020301546a398
tomkoker <teddy.koker@gmail.com>
2017-05-01 02:53:48 -0400

    back to old UI for now

--- client/components/App.vue
+++ client/components/App.vue
@@ -1,62 +1,17 @@
 <template>
-  <!-- Main Page Container -->
   <div class="container" id="app">
-
-  <!-- Left Hand Side Bar -->
-  <div class="col-sm-2">
-  <p> filler text </p>
-  </div>
-
-  <!-- Main (Center) Content -->
-  <div class="col-sm-8">
-
-    <!-- Header -->
-    <div class="page-header" id="header"> 
-      <img id="logoImg" src="/assets/logo.svg"/>
-      <h1 id="logoText">bookflip</h1> 
-    </div>
-
-    <!-- Navigation Bar -->
-    <div class="page-header" id="navbar">
-      <span> </span>
-      <span> </span>
-      <span> </span>
-    
-    </div>
-
-    <!-- REPLACE -->
-    <div>
-      <router-link v-show="!authenticated" to="/login" > <a>Login</a> </router-link>
-      <router-link v-show="!authenticated" to="/signup" > <a>Signup</a> </router-link>
-
+    <div class="page-header"><h1>bookflip</h1></div>
+      <router-link v-show="!authenticated" to="/login" ><a>Login</a></router-link>
+      <router-link v-show="!authenticated" to="/signup" ><a>Signup</a></router-link>
       <a v-show="authenticated" v-on:click="logout" href="#">Logout</a>
-    </div>
-
-    
-    <div>
       <ul class="nav nav-tabs">
-        <router-link tag="li" to="/buy" active-class="active"> <a>Buy</a> </router-link>
-        <router-link tag="li" to="/sell" active-class="active"> <a>Sell</a> </router-link>
+        <router-link tag="li" to="/buy" active-class="active"><a>Buy</a></router-link>
+        <router-link tag="li" to="/sell" active-class="active"><a>Sell</a></router-link>
       </ul>
-    </div>
-    <!-- end REPLACE -->
-
-
-    <!-- Dynamicaly Loaded Content -->
-    <div>
       <br>
       <router-view></router-view>
-    </div>
-
-  </div>
-
-  <!-- Right Hand Side Bar -->
-  <div class="col-sm-2">
-  </div>
-
   </div>
 </template>
-
 
 <script>
 
@@ -96,3 +51,4 @@ export default {
   },
 }
 </script>
+
---
+++ client/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="jumbotron">
+    <h2>Welcome</h2>
+    <p>Bookflip is the best way to buy and sell textbooks with your classmates.</p>
+  </div>
+</template>
+<script>
+
+</script>
